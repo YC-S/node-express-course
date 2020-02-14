9c00f1b24fb8670f0d977943a7e710e7d5f46088
yuanchen <yuanchenshi@gmail.com>
2020-02-14 09:48:26 -0600

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express();
 const port = 8000;
-app.get("/", (req, res) => res.send("Hello World!"));
+const mockUserData = [{ name: "Mark" }, { name: "Jill" }];
+app.get("/users", function(req, res) {
+  res.json({
+    success: true,
+    message: "successfully got users. Nice!",
+    users: mockUserData
+  });
+});
+app.get("/users", function(req, res) {
+  res.json({
+    success: true,
+    message: "successfully got users. Nice!",
+    users: mockUserData
+  });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
